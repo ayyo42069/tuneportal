d4487a150125d604484bc078fd71209c19eb00c1
kristof <kristof>
2025-02-03 11:46:04 +0100

    typo

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -82,7 +82,7 @@
                     </a>
                 </li>
                 <li>
-                <a href="manage_tuning.php" class="flex items-center p-2 hover:bg-red-700 rounded">
+                <a href="manage_tunes.php" class="flex items-center p-2 hover:bg-red-700 rounded">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                     </svg>
