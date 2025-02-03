5bf486cd0438b8639c8dcdd2f88e2eaa6838afa1
kristof <kristof>
2025-02-04 00:44:02 +0100

    csrf login teest

--- login.php
+++ login.php
@@ -83,6 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         <?php endif; ?>
         <form method="POST">
+        <?php echo csrf_input_field(); ?>
             <div class="mb-4">
                 <label class="block text-gray-700 mb-2">Email or Username</label>
                 <input type="text" name="login" required class="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-red-600">
